11a59373235cc1fbc9d011ed028c6eec986bc20b
Ian Clark <ian.harrison.clark@gmail.com>
2015-02-15 14:04:33 -0800

    committed

---
+++ wineo/ohai.c
@@ -0,0 +1,9 @@
+//
+//  ohai.c
+//  wineo
+//
+//  Created by Ian Clark on 2/15/15.
+//  Copyright (c) 2015 Ian Clark. All rights reserved.
+//
+
+#include "ohai.h"
---
+++ wineo/ohai.h
@@ -0,0 +1,14 @@
+//
+//  ohai.h
+//  wineo
+//
+//  Created by Ian Clark on 2/15/15.
+//  Copyright (c) 2015 Ian Clark. All rights reserved.
+//
+
+#ifndef __wineo__ohai__
+#define __wineo__ohai__
+
+#include <stdio.h>
+
+#endif /* defined(__wineo__ohai__) */
